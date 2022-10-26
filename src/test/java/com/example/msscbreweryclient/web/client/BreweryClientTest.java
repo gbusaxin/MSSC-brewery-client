c2416a99b6d64493bd907ff9d0d63aced7f745bf
GBu <grigorii.busakhin@software-foundation.sk>
2022-10-26 14:52:29 +0200

    Methods to send to microservice responsible for DB

--- src/test/java/com/example/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/com/example/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,6 +1,12 @@
 package com.example.msscbreweryclient.web.client;
 
+import com.example.msscbreweryclient.web.model.BeerDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,9 +14,26 @@ import static org.junit.jupiter.api.Assertions.*;
  * @created : by GBu on 26. 10. 2022, streda
  * @mailto : grigorii.busakhin@software-foundation.sk
  **/
+@SpringBootTest
 class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
 
     @Test
     void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+        assertNotNull(dto);
+    }
+
+    @Test
+    void saveNewBeer(){
+        BeerDto beerDto = BeerDto.builder().beerName("Doog").build();
+
+        URI uri = client.saveNewBeer(beerDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri);
     }
 }
